5c97e8264aa1d8745ae7f268cbc2cdfeac3e8ecc
meanzad <minzad123@gmail.com>
2024-10-17 22:21:49 +0530

    create python file for movie recommendation engine

---
+++ movie-recommendation-engine.py
@@ -0,0 +1,46 @@
+import pandas as pd
+movies = pd.read_csv("movies.csv")
+
+import re
+
+def clean_title(title):
+    return re.sub("[^a-zA-Z0-9 ]","",title)
+
+movies["clean_title"] = movies["title"].apply(clean_title)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer(ngram_range=(1,2),analyzer="char_wb")
+
+tfidf = vectorizer.fit_transform(movies['clean_title'])
+
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+def search(title):
+    title = clean_title(title)
+    query_vec = vectorizer.transform([title])
+    similarity = cosine_similarity(query_vec,tfidf).flatten()
+    indices = np.argpartition(similarity,-5)[-5:]
+    results = movies.iloc[indices][::-1]
+    return results
+
+ratings = pd.read_csv("ratings.csv")
+
+def find_similar_movies(movie_id):
+    similar_users = ratings[(ratings['movieId'] == movie_id) & (ratings["rating"] > 4)]['userId'].unique()
+    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings['rating'] > 4)]["movieId"]
+    
+    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
+    similar_user_recs = similar_user_recs[similar_user_recs > .10]
+   
+    all_users = ratings[(ratings['movieId'].isin(similar_user_recs.index)) & (ratings['rating'] > 4)]
+    all_user_recs = all_users['movieId'].value_counts() / len(all_users['userId'].unique())
+
+    rec_percentages = pd.concat([similar_user_recs,all_user_recs],axis = 1)
+    rec_percentages.columns = ["similar","all"]
+    
+    rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
+    
+    rec_percentages = rec_percentages.sort_values("score",ascending = False)
+    return rec_percentages.head(10).merge(movies,on = "movieId")[["score","title","genres"]]
