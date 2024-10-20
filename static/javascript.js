ecbdb1770b7c966f53cf6fc4e92083e6bfd60bc6
meanzad <minzad123@gmail.com>
2024-10-21 01:08:52 +0530

    improve search function to log results

--- static/javascript.js
+++ static/javascript.js
@@ -1,1 +1,24 @@
-console.log("movie recommendation system")
+function search_movie() {
+
+    var query = document.getElementById("q").value;
+    
+    fetch(`${window.origin}/search`, {
+        method: "POST",
+        body: JSON.stringify(query),
+        headers: new Headers({
+            "content-type": "application/json"
+        })
+    })
+    .then(function(response) {
+
+        if (response.status !== 200)
+        {
+            console.log(`Error ${resonse.status}`);
+            return;
+        }
+
+        response.json().then(function(data){
+            console.log(data);
+        })
+    })
+}
