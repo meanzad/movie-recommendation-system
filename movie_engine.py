ffc38fd212d99225bdd7e91cb6d825aa0497d41c
meanzad <minzad123@gmail.com>
2024-11-20 15:35:14 +0530

    remove test files

--- movie_engine.py
+++ movie_engine.py
@@ -50,7 +50,7 @@ def find_similar_movies(movie_id):
     rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
     
     rec_percentages = rec_percentages.sort_values("score",ascending = False)
-    return rec_percentages.head(10).merge(movies,on = "movieId")[["score","title","genres"]]
+    return rec_percentages.iloc[range(1,11)].merge(movies,on = "movieId")[["score","title","genres"]]
 
 
 def top_hundred():
